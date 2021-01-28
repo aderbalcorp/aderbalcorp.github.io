c1b90626d0e7f6407aa512009f272595e84f26c2
Abid Omar <contact@omarabid.com>
2021-01-28 12:59:00 +0100

    refactor(nextjs): remove nextjs config file

--- next.config.js
+++
@@ -1,6 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://raw.githubusercontent.com/aderbalcorp/aderbalcorp.github.io/gh-pages/' : '',
-}
