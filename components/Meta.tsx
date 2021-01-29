faf3b179a65db89041f11fd86f24893d1df98054
Abid Omar <contact@omarabid.com>
2021-01-29 19:22:13 +0100

    imp(meta): add viewport meta tag

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -5,6 +5,7 @@ export default function Meta(props) {
         <Head>
             <title>{props.siteTitle}</title>
             <meta name="Description" content={props.siteDescription}></meta>
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
             <link rel="icon" href="favicon.svg" />
         </Head>
     )
