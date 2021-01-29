0c6d022c2203a3dbf57f232b92228e6fa83d49d5
Abid Omar <contact@omarabid.com>
2021-01-29 22:56:50 +0100

    imp(arty): add fontawesome depdency

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 
 import "../node_modules/bootstrap/scss/bootstrap.scss"
 import '../styles/common.scss'
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />
