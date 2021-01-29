023586f027d91b7ae14a261da524196736db6fa0
Abid Omar <contact@omarabid.com>
2021-01-29 23:29:10 +0100

    imp(page): update header and footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,17 +5,15 @@ export default function Footer() {
       <div className="row align-items-center">
         <div className="col-12 col-md-6 text-center text-md-left">
           <ul className="list-inline-dash margin-bottom-10">
-            <li><a href="#">FAQ</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="#">Careers</a></li>
+            <li><a href="/terms">Terms of Service</a></li>
+            <li><a href="/privacy">Privacy Policy</a></li>
           </ul>
-          <p>© 2020 FlaTheme, All Rights Reserved.</p>
+          <p>© 2021 ADERBAL. All Rights Reserved.</p>
         </div>
         <div className="col-12 col-md-6 text-center text-md-right">
           <ul className="list-inline-sm">
-            <li><a className="button-circle button-circle-sm button-circle-white" href="#"><i className="fab fa-facebook-f" /></a></li>
-            <li><a className="button-circle button-circle-sm button-circle-white" href="#"><i className="fab fa-twitter" /></a></li>
-            <li><a className="button-circle button-circle-sm button-circle-white" href="#"><i className="fab fa-linkedin-in" /></a></li>
+            <li><a className="button-circle button-circle-sm button-circle-white icon-xl" href="#"><i className="fab fa-github" /></a></li>
+            <li><a className="button-circle button-circle-sm button-circle-white icon-xl" href="#"><i className="fab fa-hacker-news" /></a></li>
           </ul>
         </div>
       </div>
