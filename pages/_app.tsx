c53842560337717d9ded7ccb4479b7686650ba54
Abid Omar <contact@omarabid.com>
2021-01-30 02:32:44 +0100

    imp(sass): seperate styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app'
 
-import "../node_modules/bootstrap/scss/bootstrap.scss"
 import '../styles/common.scss'
 import "../node_modules/@fortawesome/fontawesome-free/css/all.css"
 
