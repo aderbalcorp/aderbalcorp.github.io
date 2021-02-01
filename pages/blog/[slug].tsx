b2566d784ed8b241a0f0d0c16832ae531e966816
Abid Omar <contact@omarabid.com>
2021-02-01 23:49:01 +0100

    imp(blog): add single post styling

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,6 +1,8 @@
 import { useRouter } from 'next/router'
 import ErrorPage from '../_error'
 
+import Layout from '../../components/Layout'
+
 import markdownToHtml from '../../lib/markdownToHtml'
 import { getAllPosts, getPostBySlug } from '../../lib/api'
 
@@ -11,9 +13,48 @@ export default function Post({ post }) {
     }
 
 return(
-    <div>
-        {post.content} 
-    </div>
+    <Layout
+        siteTitle=""
+        siteDescription=""
+    >
+        <div className="section">
+
+            <div className="container text-center">
+                <div className="row">
+                    <div className="col-12 col-lg-10 offset-lg-1 col-xl-8 offset-xl-2">
+                        <p className="font-small uppercase font-family-secondary"><a href="#">Category</a></p>
+                        <div className="margin-top-20 margin-bottom-20">
+                            <h1 className="font-weight-normal">Blog Single Standard</h1>
+                        </div>
+                        <ul className="list-inline-dash">
+                            <li><a href="#">by Admin</a></li>
+                            <li><a href="#">Feb 6, 2020</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            </div>
+
+        <div className="section bg-white">
+            <div className="container">
+                <div className="padding-y-30 padding-top-0">
+                    <div className="container text-center">
+                        <ul className="list-inline-sm">
+                            <li><span className="button button-xs button-outline-gray" href="#">#Art</span></li>
+                            <li><span className="button button-xs button-outline-gray" href="#">#Fashion</span></li>
+                            <li><span className="button button-xs button-outline-gray" href="#">#Lifestyle</span></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="row post-content"
+                    dangerouslySetInnerHTML={{ __html: post.content}}
+                >
+                </div>
+            </div>
+        </div>
+
+    </Layout>
 )
 }
 
